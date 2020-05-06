6e68396edde876df21396c7e0fd6e3c6781fcb82
Bruno <michelbruno453@hotmail.com>
2020-05-06 14:39:18 +0200

    essai 2

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 #!/usr/bin/env node
 
-console.log("Hello, Node.JS!");
+console.log(process.argv);
 
 const { getCode, getName } = require("country-list");
 console.log(getName("IS"));
+
+const ora = require("ora");
+const spinner = ora("Loading unicorns").start();
+setTimeout(() => {
+  spinner.color = "yellow";
+  spinner.text = "Loading ...";
+}, 1000);
 
 const axios = require("axios");
 axios
   .get("https://date.nager.at/api/v2/publicholidays/2020/BE")
   .then(function (response) {
-    console.log(response);
+    response.data.map(item => console.log(item.date, item.name));
   })
   .catch(function (error) {
     console.log(error);
   })
   .finally(function () {});
 
-const ora = require("ora");
-const spinner = ora("Loading unicorns").start();
-setTimeout(() => {
-  spinner.color = "yellow";
-  spinner.text = "Loading rainbows";
-}, 1000);
+spinner.stopAndPersist();
