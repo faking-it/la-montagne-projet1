17abff29c7f12525dfff857b74302fa87e00d577
Bruno <michelbruno453@hotmail.com>
2020-05-06 11:57:44 +0200

    essai holidates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-#!/bin/env node
+#!/usr/bin/env node
 
-GET / PublicHolidays / { Year } / { CountryCode };
 console.log("Hello, Node.JS!");
+
 const { getCode, getName } = require("country-list");
 console.log(getName("IS"));
+
+const axios = require("axios");
+axios
+  .get("https://date.nager.at/api/v2/publicholidays/2020/BE")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {});
