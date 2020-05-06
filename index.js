24e854933f18baad27dc91a9c644a706d1ce6bef
Bruno <michelbruno453@hotmail.com>
2020-05-06 12:12:39 +0200

    ajout ora

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ axios
     console.log(error);
   })
   .finally(function () {});
+
+const ora = require("ora");
+const spinner = ora("Loading unicorns").start();
+setTimeout(() => {
+  spinner.color = "yellow";
+  spinner.text = "Loading rainbows";
+}, 1000);
