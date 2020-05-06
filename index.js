c41fb7e4fe5937ede6d97c4dc538a9658c9f0397
Bruno <michelbruno453@hotmail.com>
2020-05-06 11:08:15 +0200

    ajout axios

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 #!/bin/env node
 
+GET / PublicHolidays / { Year } / { CountryCode };
 console.log("Hello, Node.JS!");
+const { getCode, getName } = require("country-list");
+console.log(getName("IS"));
